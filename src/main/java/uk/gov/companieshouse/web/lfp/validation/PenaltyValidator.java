4cfd2fb32201c7f98023c8ad786eaaf222ae6319
jphillips2121 <jphillips3@companieshouse.gov.uk>
2019-06-28 13:55:25 +0100

    Removed ability for penalty to contain letters

--- src/main/java/uk/gov/companieshouse/web/lfp/validation/PenaltyValidator.java
+++ src/main/java/uk/gov/companieshouse/web/lfp/validation/PenaltyValidator.java
@@ -25,7 +25,7 @@ public class PenaltyValidator implements ConstraintValidator<Penalty, String> {
         if(penaltyNumber == null || StringUtils.isEmpty(penaltyNumber)) {
             context.buildConstraintViolationWithTemplate(message).addConstraintViolation();
             return false;
-        } else if (!penaltyNumber.matches("^([a-zA-Z0-9]{8})$")) {
+        } else if (!penaltyNumber.matches("^([0-9]{8})$")) {
             context.buildConstraintViolationWithTemplate(messageNotLongEnough).addConstraintViolation();
             return false;
         }
