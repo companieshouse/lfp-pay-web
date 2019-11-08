e7a9b695d3d0e22e9a1db557224026fe8fe1f24a
jphillips2121 <jphillips3@companieshouse.gov.uk>
2019-11-08 14:37:21 +0000

    Add unit tests for start page and healthcheck

---
+++ src/test/java/uk/gov/companieshouse/web/lfp/controller/HealthcheckControllerTest.java
@@ -0,0 +1,40 @@
+package uk.gov.companieshouse.web.lfp.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class HealthcheckControllerTest {
+
+    private MockMvc mockMvc;
+
+    @InjectMocks
+    private HealthcheckController controller;
+
+    private static final String HEALTHCHECK_PATH = "/healthcheck";
+
+    @BeforeEach
+    private void setup() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    @DisplayName("Get DCA Required - success path")
+    void getRequestSuccess() throws Exception {
+
+        this.mockMvc.perform(get(HEALTHCHECK_PATH))
+                .andExpect(status().isOk());
+
+    }
+}
