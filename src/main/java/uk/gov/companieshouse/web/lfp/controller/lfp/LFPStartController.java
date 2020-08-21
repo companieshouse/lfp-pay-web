4586211bd64458cdeaa9a8b3514471ed47f25875
Faisal Amin <famin@companieshouse.gov.uk>
2020-08-21 10:39:20 +0100

    Redirect users coming from govuk to sign in page

--- src/main/java/uk/gov/companieshouse/web/lfp/controller/lfp/LFPStartController.java
+++ src/main/java/uk/gov/companieshouse/web/lfp/controller/lfp/LFPStartController.java
@@ -1,12 +1,15 @@
 package uk.gov.companieshouse.web.lfp.controller.lfp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 import uk.gov.companieshouse.api.model.latefilingpenalty.FinanceHealthcheck;
 import uk.gov.companieshouse.api.model.latefilingpenalty.FinanceHealthcheckStatus;
 import uk.gov.companieshouse.web.lfp.annotation.NextController;
@@ -17,11 +20,15 @@ import uk.gov.companieshouse.web.lfp.service.latefilingpenalty.LateFilingPenalty
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Optional;
 
 @Controller
 @NextController(EnterLFPDetailsController.class)
 @RequestMapping("/late-filing-penalty")
 public class LFPStartController extends BaseController {
+
+    @Value("${account.url}")
+    private String accountsUrl;
 
     private static String LFP_TEMP_HOME = "lfp/home";
     private static String LFP_SERVICE_UNAVAILABLE = "lfp/serviceUnavailable";
@@ -38,7 +45,7 @@ public class LFPStartController extends BaseController {
     private Environment environment;
 
     @GetMapping
-    public String getLFPHome(Model model) throws ParseException {
+    public String getLFPHome(@RequestParam("start") Optional<Integer> startId, Model model) throws ParseException {
 
         FinanceHealthcheck financeHealthcheck;
         try {
@@ -48,6 +55,10 @@ public class LFPStartController extends BaseController {
         }
 
         if (financeHealthcheck.getMessage().equals(FinanceHealthcheckStatus.HEALTHY.getStatus())) {
+            if(startId.isPresent() && startId.get() == 0) {
+                return navigatorService.getNextControllerRedirect(this.getClass());
+            }
+
             return getTemplateName();
         } else if (financeHealthcheck.getMessage().equals(FinanceHealthcheckStatus.UNHEALTHY_PLANNED_MAINTENANCE.getStatus())) {
             DateFormat inputDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
