0d86e5fe0fec1ddd99154f418101e52dead3bcc2
daisysanson <dsanson@companieshouse.gov.uk>
2024-06-10 13:52:04 +0100

    remove deprecated class in favour of new java web security syntax

--- src/main/java/uk/gov/companieshouse/web/lfp/security/WebSecurity.java
+++ src/main/java/uk/gov/companieshouse/web/lfp/security/WebSecurity.java
@@ -1,10 +1,9 @@
 package uk.gov.companieshouse.web.lfp.security;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import uk.gov.companieshouse.auth.filter.HijackFilter;
 import uk.gov.companieshouse.auth.filter.UserAuthFilter;
@@ -14,52 +13,51 @@ import uk.gov.companieshouse.session.handler.SessionHandler;
 @EnableWebSecurity
 public class WebSecurity {
 
-    @Configuration
     @Order(1)
-    public static class TemporaryStartPageSecurityConfig extends WebSecurityConfigurerAdapter {
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http
-                    .antMatcher("/late-filing-penalty");
-        }
+    public SecurityFilterChain temporaryStartPageSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/late-filing-penalty")
+                .authorizeRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                );
+        return http.build();
     }
 
 
-    @Configuration
+
     @Order(2)
-    public static class AccessibilityStatementPageSecurityConfig extends WebSecurityConfigurerAdapter {
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http
-                    .antMatcher("/late-filing-penalty/accessibility-statement");
-        }
+    protected SecurityFilterChain accessibilityStatementPageSecurityConfig(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/late-filing-penalty/accessibility-statement")
+                .authorizeRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                );
+        return http.build();
     }
 
 
-    @Configuration
     @Order(3)
-    public static class HealthcheckSecurityConfig extends WebSecurityConfigurerAdapter {
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.antMatcher("/late-filing-penalty/healthcheck");
-        }
+    protected SecurityFilterChain healthcheckSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/late-filing-penalty/healthcheck")
+                .authorizeRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                );
+        return http.build();
     }
 
-    @Configuration
+
     @Order(4)
-    public static class LFPWebSecurityFilterConfig extends WebSecurityConfigurerAdapter {
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-
-            http.antMatcher("/late-filing-penalty/**")
-                    .addFilterBefore(new SessionHandler(), BasicAuthenticationFilter.class)
-                    .addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class)
-                    .addFilterBefore(new UserAuthFilter(), BasicAuthenticationFilter.class);
-        }
+    public SecurityFilterChain lFPWebSecurityFilterConfig (HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/late-filing-penalty/**")
+                .addFilterBefore(new SessionHandler(), BasicAuthenticationFilter.class)
+                .addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class)
+                .addFilterBefore(new UserAuthFilter(), BasicAuthenticationFilter.class)
+                .authorizeRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                );
+        return http.build();
     }
 
 }
